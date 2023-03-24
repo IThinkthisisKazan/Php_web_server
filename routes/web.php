f03eba1a3416b7fbac40e9fe8c8836d36f5a624e
Ильяс Нугаев <i.nugaev@nuzhnapomosh.ru>
2023-03-24 22:13:27 +0300

    init laravel10 with vue3

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TextController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,14 +8,11 @@ use App\Http\Controllers\TextController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', [MyController::class, 'index']);
-
-Route::get('/student', [StudentController::class, 'index']);
-
-Route::get('/text', [TextController::class, 'index']);
-
+Route::get('/', function () {
+    return view('welcome');
+});
